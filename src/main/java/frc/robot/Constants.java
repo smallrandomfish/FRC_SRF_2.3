d7a8d12d4d1b7589f36f68dd4239f0f563e3ea7b
= <=>
2024-12-31 13:16:25 -0500

    start

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+//Driver
+public class Constants {
+    public class oi { // this is for "operator interface"
+      public static final int Driver = 0;
+      public static final int Operator = 1;
+    }
+
+
+    //Op
+    public class shooter {
+        
+    }
+    }
